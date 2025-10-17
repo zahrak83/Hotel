e61390c68568b042b7cfd9db2b5cb6823f62b356
Zahra <zaahrra8305@gmail.com>
2025-10-17 14:26:27 +0330

    5

--- Hotel/Dtos/RoomDetailCreateDto.cs
+++ Hotel/Dtos/RoomDetailCreateDto.cs
@@ -2,7 +2,6 @@
 {
     public class RoomDetailCreateDto
     {
-        public int RoomId { get; set; }
         public string Description { get; set; }
         public bool HasWifi { get; set; }
         public bool HasAirConditioner { get; set; }
